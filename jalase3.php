b84a3c9273b279b0972e691cf82f4075b1e7abe3
kamyar <kami6535@gmail.com>
2024-04-23 12:35:27 +0330

    php kamyar

---
+++ jalase3.php
@@ -0,0 +1,23 @@
+<html>
+<style>
+*{
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+    }
+
+
+h2{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: blue;
+    font-size: 100px;
+}
+
+
+</style>
+<h2> php poroject1  </h2>
+
+
+</html>
