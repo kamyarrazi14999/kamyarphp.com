f5ac6ffc197d044f9f784b39ef31cde01956b33f
kamyar <kami6535@gmail.com>
2024-05-07 18:13:05 +0330

    math php

---
+++ jalase5/object.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class person {
+
+public $firstName='Kamiyar';
+
+public $lastName = 'Razi';
+
+
+}
+// ? class php
+
+$persion= new person();
+
+echo $persion -> firstName ,' <br>';
+echo $persion -> lastName;
+
+
+?>
