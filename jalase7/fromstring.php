36aeb30d2d58fd60ffd819e3a4b5f88c2e4b6964
kamyar <kami6535@gmail.com>
2024-05-22 20:03:10 +0330

    master

---
+++ jalase7/fromstring.php
@@ -0,0 +1,42 @@
+<style>
+textarea{
+font-size: medium;
+font-weight: 200;
+font-style: italic;
+text-align: center;
+align-items: center;
+background:#0d7335 ;
+}
+
+
+</style>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form php</title>
+</head>
+<body>
+ <form action="" method="$_POST">
+<label for=""> name </label>
+<textarea type="text" name="text" id="" rows="10" cols="80"></textarea>
+<input type="submit">
+</form>   
+</body>
+</html>
+<?php
+$text= '';
+if($_SERVER["REQUEST_METHOD"]==='POST'){
+
+
+
+}
+
+
+
+
+?>
