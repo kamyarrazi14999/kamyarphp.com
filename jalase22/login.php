bb2bfb6af6ace287f55c133e5f5ed5613ea1a78a
kamyarrazi14999 <kami6535@gmail.com>
2024-10-28 13:43:33 +0330

    key code

--- jalase22/login.php
+++ jalase22/login.php
@@ -33,7 +33,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
             ];
             $jwt = JWT::encode($payload, 'secret_key', 'HS256');
-            $setcookie("auth_token");
+            $setcookie("auth_token",$jwt, time() + 3600, '/', null, false, false);
 
              
         }
