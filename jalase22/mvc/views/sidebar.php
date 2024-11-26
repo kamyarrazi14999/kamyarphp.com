4ced6ba415e8ec5e6effc560bc776cae9925a097
kamyarrazi14999 <kami6535@gmail.com>
2024-11-26 17:24:53 +0330

    mvc

---
+++ jalase22/mvc/views/sidebar.php
@@ -0,0 +1,16 @@
+<div class="sidebar">
+   <div class="sidebar-header">
+    <h2>Welcome <?php echo "Admin"; ?></h2>
+</div>
+<ul>
+   <li><a href=""></a>Dashboard</li>
+   <li><a href=""></a> Blog</li>
+   <li><a href=""></a>Page</li>
+   <li><a href=""></a>Users</li>
+   <li><a href=""></a>Media</li>
+   <li><a href=""></a>Product </li>
+   <li><a href=""></a>Orders</li>
+   <li><a href=""> </a>Settings </li>
+</ul>
+
+</div>
