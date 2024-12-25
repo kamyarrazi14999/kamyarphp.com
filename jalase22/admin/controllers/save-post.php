9dd1c683e05ddd5243b5ea1e5b89f3ba72f69112
kamyarrazi14999 <kami6535@gmail.com>
2024-12-25 23:47:14 +0330

    update save post

--- jalase22/admin/controllers/save-post.php
+++ jalase22/admin/controllers/save-post.php
@@ -14,9 +14,32 @@ $user_id = $_SESSION['user_id'] ?? ''  ;
  }
  $stmt = $db->prepare ("SELECT post_id FROM blog_posts WHERE title = :title AND user_id = :user_id");
  $stmt->execute(['title' => $titel, 'user_id' => $user_id]);
-$exists_post = $stmt->fetch(PDO::FETCH_ASSOC);    
+$existing_post = $stmt->fetch(PDO::FETCH_ASSOC); 
+if ($existing_post){
+    // update the post
+$stmt = $db->prepare (' UPDATE blog_posts SET content = :content WHERE post_id = :post_id');
+$stmt->execute(['content' => $content, 'post_id' => $existing_post['post_id']]);
+$_SESSION['massage'] = "$title با موفقیت بروزرسانی شد";
+    
+} 
+else {
+    // insert post
+    $stmt = $db->prepare ('INSERT INTO blog_posts (title, content, user_id) VALUES (:title, :content, :user_id)');
+    $stmt->execute([' title'=>$title, 'content'=>$content, 'user_id'=>$user_id ]);
+    $_SESSION['massage'] = "$title با موفقیت ثبت شد";
+} 
+// ذخیره اطلاعات در متن برای نمایش مجدد به کاربر در فرم 
+$_SESSION['title'] = $titel;
+$_SESSION['content'] = $content;
+header('location: ../views/created_blog.php');
+ exit();  
+
 
 } catch (Exception $e) {
+    $_SESSION['massage'] = 'خطا در ذخیره سازسی پست '.$e->getMessage();
+    header('location:../views/created_blog.php');
+    exit();
+
 }
 
 ?>
--- jalase22/admin/views/created_blog.php
+++ jalase22/admin/views/created_blog.php
@@ -1,6 +1,8 @@
 <?php
 include '../controllers/cheaklogin.php';
 include '../../database.php';
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
